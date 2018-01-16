99ffdd5d506224d3015fdb68e951773672a28d80
Dirk Becker-Horst <dirk@becker-horst.com>
2018-01-16 19:00:35 +0100

    Added game of life

--- glowcube.cc
+++ glowcube.cc
@@ -21,35 +21,167 @@ static void InterruptHandler(int signo) {
     interrupt_received = true;
 }
 
-static void DrawOnCanvas(Canvas *canvas) {
-    /*
-     * Let's create a simple animation. We use the canvas to draw
-     * pixels. We wait between each step to have a slower animation.
-     */
-    canvas->Fill(0, 0, 255);
+// Conway's game of life
+// Contributed by: Vliedel
+class GameLife : public ThreadedCanvasManipulator {
+public:
+  GameLife(Canvas *m, int delay_ms=500, bool torus=true)
+    : ThreadedCanvasManipulator(m), delay_ms_(delay_ms), torus_(torus) {
+    width_ = canvas()->width();
+    height_ = canvas()->height();
 
-    int center_x = canvas->width() / 2;
-    int center_y = canvas->height() / 2;
-    float radius_max = canvas->width() / 2;
-    float angle_step = 1.0 / 360;
-    for (float a = 0, r = 0; r < radius_max; a += angle_step, r += angle_step) {
-        if (interrupt_received)
-            return;
-        float dot_x = cos(a * 2 * M_PI) * r;
-        float dot_y = sin(a * 2 * M_PI) * r;
-        canvas->SetPixel(center_x + dot_x, center_y + dot_y,
-                         255, 0, 0);
-        usleep(1 * 1000);  // wait a little to slow down things.
+    // Allocate memory
+    values_ = new int*[width_];
+    for (int x=0; x<width_; ++x) {
+      values_[x] = new int[height_];
     }
-}
+    newValues_ = new int*[width_];
+    for (int x=0; x<width_; ++x) {
+      newValues_[x] = new int[height_];
+    }
+
+    // Init values randomly
+    srand(time(NULL));
+    for (int x=0; x<width_; ++x) {
+      for (int y=0; y<height_; ++y) {
+        values_[x][y]=rand()%2;
+      }
+    }
+    r_ = rand()%255;
+    g_ = rand()%255;
+    b_ = rand()%255;
+
+    if (r_<150 && g_<150 && b_<150) {
+      int c = rand()%3;
+      switch (c) {
+      case 0:
+        r_ = 200;
+        break;
+      case 1:
+        g_ = 200;
+        break;
+      case 2:
+        b_ = 200;
+        break;
+      }
+    }
+  }
+
+  ~GameLife() {
+    for (int x=0; x<width_; ++x) {
+      delete [] values_[x];
+    }
+    delete [] values_;
+    for (int x=0; x<width_; ++x) {
+      delete [] newValues_[x];
+    }
+    delete [] newValues_;
+  }
+
+  void Run() {
+    while (running() && !interrupt_received) {
+
+      updateValues();
+
+      for (int x=0; x<width_; ++x) {
+        for (int y=0; y<height_; ++y) {
+          if (values_[x][y])
+            canvas()->SetPixel(x, y, r_, g_, b_);
+          else
+            canvas()->SetPixel(x, y, 0, 0, 0);
+        }
+      }
+      usleep(delay_ms_ * 1000); // ms
+    }
+  }
+
+private:
+  int numAliveNeighbours(int x, int y) {
+    int num=0;
+    if (torus_) {
+      // Edges are connected (torus)
+      num += values_[(x-1+width_)%width_][(y-1+height_)%height_];
+      num += values_[(x-1+width_)%width_][y                    ];
+      num += values_[(x-1+width_)%width_][(y+1        )%height_];
+      num += values_[(x+1       )%width_][(y-1+height_)%height_];
+      num += values_[(x+1       )%width_][y                    ];
+      num += values_[(x+1       )%width_][(y+1        )%height_];
+      num += values_[x                  ][(y-1+height_)%height_];
+      num += values_[x                  ][(y+1        )%height_];
+    }
+    else {
+      // Edges are not connected (no torus)
+      if (x>0) {
+        if (y>0)
+          num += values_[x-1][y-1];
+        if (y<height_-1)
+          num += values_[x-1][y+1];
+        num += values_[x-1][y];
+      }
+      if (x<width_-1) {
+        if (y>0)
+          num += values_[x+1][y-1];
+        if (y<31)
+          num += values_[x+1][y+1];
+        num += values_[x+1][y];
+      }
+      if (y>0)
+        num += values_[x][y-1];
+      if (y<height_-1)
+        num += values_[x][y+1];
+    }
+    return num;
+  }
+
+  void updateValues() {
+    // Copy values to newValues
+    for (int x=0; x<width_; ++x) {
+      for (int y=0; y<height_; ++y) {
+        newValues_[x][y] = values_[x][y];
+      }
+    }
+    // update newValues based on values
+    for (int x=0; x<width_; ++x) {
+      for (int y=0; y<height_; ++y) {
+        int num = numAliveNeighbours(x,y);
+        if (values_[x][y]) {
+          // cell is alive
+          if (num < 2 || num > 3)
+            newValues_[x][y] = 0;
+        }
+        else {
+          // cell is dead
+          if (num == 3)
+            newValues_[x][y] = 1;
+        }
+      }
+    }
+    // copy newValues to values
+    for (int x=0; x<width_; ++x) {
+      for (int y=0; y<height_; ++y) {
+        values_[x][y] = newValues_[x][y];
+      }
+    }
+  }
+
+  int** values_;
+  int** newValues_;
+  int delay_ms_;
+  int r_;
+  int g_;
+  int b_;
+  int width_;
+  int height_;
+  bool torus_;
+};
 
 int main(int argc, char *argv[]) {
     RGBMatrix::Options defaults;
     defaults.hardware_mapping = "regular";  // or e.g. "adafruit-hat"
-    defaults.rows = 32;
-    defaults.chain_length = 1;
+    defaults.rows = 64;
+    defaults.chain_length = 12;
     defaults.parallel = 1;
-    defaults.show_refresh_rate = true;
+    defaults.show_refresh_rate = false;
     Canvas *canvas = rgb_matrix::CreateMatrixFromFlags(&argc, &argv, &defaults);
     if (canvas == NULL)
         return 1;
@@ -60,11 +192,32 @@ int main(int argc, char *argv[]) {
     signal(SIGTERM, InterruptHandler);
     signal(SIGINT, InterruptHandler);
 
-    DrawOnCanvas(canvas);    // Using the canvas.
+    int scroll_ms = 30;
+
+    ThreadedCanvasManipulator *image_gen = new GameLife(canvas, scroll_ms);
+    image_gen->Start();
+
+    // Now, the image generation runs in the background. We can do arbitrary
+    // things here in parallel. In this demo, we're essentially just
+    // waiting for one of the conditions to exit.
+    int runtime_seconds = -1;
+    if (runtime_seconds > 0) {
+      sleep(runtime_seconds);
+    } else {
+      // The
+      printf("Press <CTRL-C> to exit and reset LEDs\n");
+      while (!interrupt_received) {
+        sleep(1); // Time doesn't really matter. The syscall will be interrupted.
+      }
+    }
 
     // Animation finished. Shut down the RGB matrix.
     canvas->Clear();
+    delete image_gen;
     delete canvas;
+
+    rintf("\%s. Exiting.\n",
+         interrupt_received ? "Received CTRL-C" : "Timeout reached");
 
     return 0;
 }
